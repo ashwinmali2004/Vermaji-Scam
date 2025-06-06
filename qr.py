93e08f5f54ad1a1705acd4a2ee95f83e6c92f772
Ashwinmali2004 <Ashwinmali2004@gmail.com>
2025-06-06 16:21:50 +0530

    Done!

--- qr.py
+++ qr.py
@@ -1,3 +1,22 @@
-import qrcode as qr
-img=qr.make("")
-img.save("nptel.jpg")
+# Method - 2 to make the QRCode of our choice!!
+import qrcode
+from PIL import Image
+
+# Create QR code instance
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_H,
+    box_size=10,
+    border=4,
+)
+
+# Add data to the QR code
+data = "https://nptel24cs19s35780068630789867.netlify.app/"
+qr.add_data(data)
+qr.make(fit=True)
+
+# Create an image from the QR Code instance
+img = qr.make_image(fill_color="black", back_color="white")
+
+# Save the image
+img.save("nptel24cs19s35780068630789867.jpg")
